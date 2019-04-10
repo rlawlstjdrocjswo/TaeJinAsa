ae2088481106ba5a8c828ef87c2d55a9c64f0e9b
KimJinSeong <ginseong21@gmail.com>
2019-04-10 10:10:23 +0900

    Initial commit

---
+++ app/src/main/java/com/example/test0403/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.test0403;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+    }
+}
